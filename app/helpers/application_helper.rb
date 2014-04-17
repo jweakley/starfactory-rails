532e7852a40503cc4a4c102f757a54e0d90ae2d9
Douglas Waltman II <doug@dougwaltman.com>
2014-04-16 21:30:01 -0700

    Added gems and fleshed out views and css stuffs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+
+  def body_classes
+    classes = [
+      under_camel(params[:controller]),
+      under_camel(params[:action]),
+      under_camel("#{params[:controller]}_#{params[:action]}")
+    ]
+    classes.push 'hasQueryString' unless request.query_string.blank?
+    return classes
+  end
+
+  def under_camel(string)
+    string.gsub(/[^a-z]/i,'_').camelize(:lower)
+  end
 end
